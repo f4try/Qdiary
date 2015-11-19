fd812503da268e1a28ac1b961446b62223574c00
Zheng Zong <Zheng Zong>
2015-11-19 14:41:58 +0800

    Add motion title

--- about.py
+++ about.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def about():
-	return "Qdiary Project Started on 2015.11.17"
+	# return "Qdiary Project Started on 2015.11.17"
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8080)
